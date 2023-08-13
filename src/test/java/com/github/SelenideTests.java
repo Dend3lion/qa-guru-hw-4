aec69539073f9105048631c842c2be743d2e9f27
Denis Bashkatov <dend3lion@gmail.com>
2023-08-13 15:30:53 +0300

    test: softAssertions selenide wiki test

---
+++ src/test/java/com/github/SelenideTests.java
@@ -0,0 +1,39 @@
+package com.github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void checkJUnitTests() {
+        open("selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-box").$(".wiki-more-pages-link").$("button").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+}
